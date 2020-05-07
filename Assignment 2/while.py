8c3b68793adf02943f36c9f64b93218f2e6c7e86
Yu Ji <jessejiyu@outlook.com>
2020-05-06 20:46:34 -0700

    easy cases

--- Assignment 2/while.py
+++ Assignment 2/while.py
@@ -1,16 +1,22 @@
-def main():
-    text = input().split()
-    ast = build(text)
-    result = str(calculate(ast))
-    print("{ ", result, " }")
+import numpy as np
 
+class Node:
+    def __init__(self, key = None, num=0, tf=True, tp=None):
+        self.left = None
+        self.right = None
+        self.num = num
+        self.key = key
+        self.tf = tf
+        self.tp = tp
 
-if __name__ == '__main__':
-    main()
+    def printNode(self):
+        print("num: ", num, " key: ", key, " tf: ", tf, " type: ", tp)
 
 vars = []
 outVars = []
 lines = []
+whileArr = []
+parseTree = Node()
 add = "+"
 minus = "-"
 multi = "*"
@@ -18,53 +24,99 @@ equal = ":="
 he = "∧"
 huo = "∨"
 no = "¬"
-equalout = "→"
-
-
-def calculate(ast):
-    for var in outVars:
 
 
 def build(text):
     divString(text)
     for line in lines:
-        parse(line)
+        split(line)
 
 
-def parse(text):
+def split(text):
+    global parseTree
     if text[0] == "if":
-        ifthen(text)
+        parseTree = ifthen(text)
     elif text[0] == "while":
-        whiledo(text)
+        parseTree = whiledo(text)
     elif text[0] == "skip":
         pass
     else:
         nameVar(text)
 
 
+# def parseSub(text):
+#     if text[0] == "if":
+#         ifthen(text)
+#     elif text[0] == "while":
+#         whiledo(text)
+
+def parse(text):
+    global outVars
+    if text[0] == "(" and text[len(text)-1] == ")":
+        text = text[1:len(text) - 1]
+    if text[0] == no:
+        return not parse(text[1:])
+    if len(text) == 1:
+        if text[0] == "true":
+            return True
+        else:
+            return False
+    if len(text) == 3 and text[1] == ":=":
+        nameVar(text)
+    if len(text) == 3 and text[1] == "=":
+        var = text[:text.index("=")][0]
+        val = text[text.index("=") + 1:][0]
+        outVars = np.transpose(np.asarray(outVars))
+        if var not in outVars[0]:
+            outVars = np.transpose(outVars)
+            outVars = outVars.tolist()
+            return val == 0
+        else:
+            id = np.where(outVars[0] == var)
+            val_stored = outVars[1][id]
+            outVars = np.transpose(outVars)
+            outVars = outVars.tolist()
+            return val == val_stored
+    return True
+    # if he in text or huo in text:
+
+
+
 def ifthen(text):
     arrif = text[text.index("if")+1:text.index("then")]
     arrthen = text[text.index("then")+1:text.index("else")]
     arrelse = text[text.index("else")+1:]
-    return arrif, arrthen, arrelse
+    # print(arrif)
+    # print(arrthen)
+    # print(arrelse)
+    ast = Node(key=arrif, tp="ifthenP")
+    ast.left = Node(key=arrthen, tp="ifthenL")
+    ast.right = Node(key=arrelse, tp="ifthenR")
+    return ast
 
 
 def whiledo(text):
     arrwhile = text[text.index("while")+1:text.index("do")]
     arrdo = text[text.index("do")+1:]
-    return arrwhile, arrdo
+    ast = Node(tp="whiledoP")
+    ast.left = Node(key=arrwhile, tp="whiledoL")
+    ast.right = Node(key=arrdo, tp="whiledoR")
+    return ast
 
 
-class Node :
-    def __init__(self, key, num=0, tf=True):
-        self.left = None
-        self.right = None
-        self.num = num
-        self.key = key
-        self.tf = tf
+def parse_tree(curr):
+    if curr.tp == "ifthenP":
+        parse(curr.key)
+        if curr.tf:
+            parse(curr.left.key)
+        else:
+            parse(curr.right.key)
+    if curr.tp == "whiledoP":
+        parse(curr.left.key)
+        while curr.left.tf:
+            parse(curr.right.key)
 
-    def printNode(self):
-        print("num: ", num, " key: ", key, " tf: ", tf)
+
 
 
 def clear():
@@ -73,20 +125,55 @@ def clear():
 
 
 def nameVar(text):
-    var = text[:text.index("=")][0]
-    val = text[text.index("=")+1:][0]
-    vars.append([var, int(val)])
+    global outVars
+    var = text[:text.index(":=")][0]
+    val = text[text.index(":=")+1:][0]
+    # print("var:", var)
+    # print("val:", val)
+    outVars = np.transpose(np.asarray(outVars))
+    # print("outVars:", outVars)
+    if len(outVars) == 0 or var not in outVars[0]:
+        outVars = np.transpose(outVars)
+        outVars = outVars.tolist()
+        outVars.append([var, int(val)])
+    else:
+        id = np.where(outVars[0] == var)
+        outVars[1][id] = val
+        outVars = np.transpose(outVars)
+        outVars = outVars.tolist()
 
 
 def divString(text):
     count = text.count(";")
-    if count > 0:
+    if count == 0:
+        lines.append(text)
+    else:
         count = count + 1
-    while count > 0:
-        if ";" in text:
-            subText = text[:text.index(";")]
-            text = text[text.index(";")+1:]
-        else:
-            subText = text
-        lines.append(subText)
-        count = count - 1
+        while count > 0:
+            if ";" in text:
+                subText = text[:text.index(";")]
+                text = text[text.index(";")+1:]
+            else:
+                subText = text
+            lines.append(subText)
+            count = count - 1
+
+
+
+def printAnw():
+    output = ""
+    if outVars:
+        for var in outVars:
+            output += var[0] + " → " + str(var[1]) + ", "
+    output = output[:-2]
+    return output
+
+def main():
+    text = input().split()
+    build(text)
+    parse_tree(parseTree)
+    res = printAnw()
+    print("{" + res + "}")
+
+if __name__ == '__main__':
+    main()
