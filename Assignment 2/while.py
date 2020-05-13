ec20a5fb8b53107ac44ff576b50b6b6e0ef3e55d
Yu Ji <jessejiyu@outlook.com>
2020-05-12 19:15:32 -0700

    medium 0.9

--- Assignment 2/while.py
+++ Assignment 2/while.py
@@ -1,16 +1,15 @@
 import numpy as np
 
 class Node:
-    def __init__(self, key=None, num=0, tf=True, tp=None):
+    def __init__(self, key=None, tf=True, tp=None):
         self.left = None
         self.right = None
-        self.num = num
         self.key = key
         self.tf = tf
         self.tp = tp
 
     def printNode(self):
-        print("num: ", num, " key: ", key, " tf: ", tf, " type: ", tp)
+        print(" key: ", self.key, " tf: ", self.tf, " type: ", self.tp)
 
 vars = []
 outVars = []
@@ -61,14 +60,14 @@ def parse(text):
     if "∧" in text:
         return parse(text[:text.index("∧")]) and parse(text[text.index("∧")+1:])
 
-    if len(text) == 3 and "<" in text and is_int(text[0]) and is_int(text[2]):
-        return int(text[0]) < int(text[2])
-    if len(text) == 3 and ">" in text and is_int(text[0]) and is_int(text[2]):
-        return int(text[0]) > int(text[2])
-    if len(text) == 3 and "=" in text and is_int(text[0]) and is_int(text[2]):
-        return int(text[0]) == int(text[2])
+    # if len(text) == 3 and "<" in text and is_int(text[0]) and is_int(text[2]):
+    #     return int(text[0]) < int(text[2])
+    # if len(text) == 3 and ">" in text and is_int(text[0]) and is_int(text[2]):
+    #     return int(text[0]) > int(text[2])
+    # if len(text) == 3 and "=" in text and is_int(text[0]) and is_int(text[2]):
+    #     return int(text[0]) == int(text[2])
 
-    if len(text) == 3 and ("<" or ">") in text:
+    if len(text) == 3:
         return compare(text[0], text[2], text[1])
 
 
@@ -104,7 +103,13 @@ def whiledo(text):
     arrdo = text[text.index("do")+1:]
     ast = Node(tp="whiledoP")
     ast.left = Node(key=arrwhile, tp="whiledoL")
-    ast.right = Node(key=arrdo, tp="whiledoR")
+
+    if "{" and "}" in arrdo:
+        arrdo = arrdo[1:len(arrdo) - 1]
+        if arrdo[0] == "if":
+            ast.right = ifthen(arrdo)
+    else:
+        ast.right = Node(key=arrdo, tp="whiledoR")
     return ast
 
 
@@ -112,6 +117,7 @@ def parse_tree(curr):
     global outVars
     if curr.tp == "ifthenP":
         curr.tf = parse(curr.key)
+        # print(curr.tf)
         if curr.tf:
             parse(curr.left.key)
         else:
@@ -120,8 +126,14 @@ def parse_tree(curr):
         curr.left.tf = parse(curr.left.key)
         while curr.left.tf:
             before = outVars
-            curr.left.tf = parse(curr.right.key)
+            print("before: ", before)
+            if curr.right.tp == "ifthenP":
+                parse_tree(curr.right)
+                curr.left.tf = parse(curr.left.key)
+            else:
+                curr.left.tf = parse(curr.right.key)
             after = outVars
+            print("after: ", after)
             if before == after:
                 break
 
@@ -137,13 +149,16 @@ def nameVar(text):
     val = text[text.index(":=")+1:]
     if len(val) == 1:
         val = val[0]
-    if len(val) == 3:
+        val = read_val(val)
+    elif len(val) == 3:
+        val_a = read_val(val[0])
+        val_b = read_val(val[2])
         if val[1] == add:
-            val = int(val[0]) + int(val[2])
+            val = val_a + val_b
         elif val[1] == minus:
-            val = int(val[0]) - int(val[2])
+            val = val_a - val_b
         elif val[1] == multi:
-            val = int(val[0]) * int(val[2])
+            val = val_a * val_b
     check_exist(var, val)
 
 
@@ -195,58 +210,57 @@ def check_exist(var, val):
         outVars = outVars.tolist()
 
 
-def compare(a, b, sign):
+def read_val(var):
     global outVars
-    outVars = np.transpose(np.asarray(outVars))
+    if is_int(var):
+        return int(var)
+    else:
+        outVars = np.transpose(np.asarray(outVars))
+        if len(outVars) == 0 or var not in outVars[0]:
+            outVars = np.transpose(outVars)
+            outVars = outVars.tolist()
+            return 0
+        else:
+            id = np.where(outVars[0] == var)
+            val = outVars[1][id]
+            outVars = np.transpose(outVars)
+            outVars = outVars.tolist()
+            return int(val)
 
+
+def compare(a, b, sign):
     if is_int(a):
-        var = b
+        val_a = int(a)
+        val_b = read_val(b)
+    elif is_int(b):
+        val_a = read_val(a)
+        val_b = int(b)
     else:
-        var = a
+        val_a = read_val(a)
+        val_b = read_val(b)
 
-    if len(outVars) == 0 or var not in outVars[0]:
-        outVars = np.transpose(outVars)
-        outVars = outVars.tolist()
-        if is_int(a):
-            if sign == "<":
-                return int(a) < 0
-            elif sign == ">":
-                return int(a) > 0
-            elif sign == "=":
-                return 0 == int(a)
-        else:
-            if sign == "<":
-                return 0 < int(b)
-            elif sign == ">":
-                return 0 > int(b)
-            elif sign == "=":
-                return 0 == int(b)
-    else:
-        id = np.where(outVars[0] == var)
-        val = outVars[1][id][0]
-        outVars = np.transpose(outVars)
-        outVars = outVars.tolist()
-        if is_int(a):
-            if sign == "<":
-                return int(a) < int(val)
-            elif sign == ">":
-                return int(a) > int(val)
-            elif sign == "=":
-                return int(val) == int(a)
-        else:
-            if sign == "<":
-                return int(val) < int(b)
-            elif sign == ">":
-                return int(val) > int(b)
-            elif sign == "=":
-                return int(val) == int(b)
+    if sign == "=":
+        return val_a == val_b
+    elif sign == "<":
+        return val_a < val_b
+    elif sign == ">":
+        return val_a > val_b
+
     return False
+
+
+def print_tree(curr):
+    if curr:
+        print_tree(curr.left)
+        curr.printNode()
+        print_tree(curr.right)
 
 
 def main():
     text = input().split()
     build(text)
     parse_tree(parseTree)
+    print_tree(parseTree)
     res = printAnw()
     print("{" + res + "}")
 
